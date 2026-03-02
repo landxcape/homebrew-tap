9cf67219ec9d2517f38d1c87fecd9df981cab11c
Purna Kalyan Shakya <38076182+landxcape@users.noreply.github.com>
2026-03-02 16:29:36 +0545

    camgylph 1.4.0

--- Formula/camgylph.rb
+++ Formula/camgylph.rb
@@ -1,8 +1,8 @@
 class Camgylph < Formula
   desc "Real-time CLI camera renderer that converts webcam frames into colored ASCII"
   homepage "https://github.com/landxcape/camgylph"
-  url "https://github.com/landxcape/camgylph/archive/refs/tags/v1.3.2.tar.gz"
-  sha256 "47039041fc332d25e6c1b472bd96a3581c18256d46e836429e32921432f8aee1"
+  url "https://github.com/landxcape/camgylph/archive/refs/tags/v1.4.0.tar.gz"
+  sha256 "7f022bb009bd20393fb9286b53b38e575a82ac2fd0c67f5bfc3df452816d984f"
   license "MIT"
 
   depends_on "rust" => :build
