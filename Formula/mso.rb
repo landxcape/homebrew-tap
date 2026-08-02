609cd4f0532060e7519ab23ae6c814b9be126450
Purna Kalyan Shakya <38076182+landxcape@users.noreply.github.com>
2026-08-02 09:26:51 +0545

    fix(mso): update arm64 sha256 checksum for v0.3.1

--- Formula/mso.rb
+++ Formula/mso.rb
@@ -7,7 +7,7 @@ class Mso < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/landxcape/mac-sym-offload/releases/download/v0.3.1/mso-macos-arm64.tar.gz"
-      sha256 "beb064ec57a9f9cab6d1bb34e017ba91fce2a74d0c91a1621d17598dab70c96d"
+      sha256 "5126301083e5a73a73f4931d888a7bde7f3e94b6817557549d7baf4ad53c2327"
     elsif Hardware::CPU.intel?
       url "https://github.com/landxcape/mac-sym-offload/releases/download/v0.3.1/mso-macos-x86_64.tar.gz"
       sha256 "e2fe692456268679f37c989d28c480c38de16f854964b8b1c0ed8f0c8b6eebfe"
