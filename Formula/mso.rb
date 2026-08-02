7515e48a72edcac99752be7f6347df6d680c91b8
Purna Kalyan Shakya <38076182+landxcape@users.noreply.github.com>
2026-08-02 14:43:26 +0545

    feat(mso): update mso formula to v0.4.1

--- Formula/mso.rb
+++ Formula/mso.rb
@@ -1,16 +1,16 @@
 class Mso < Formula
   desc "Safely offload bloated macOS developer caches to an external APFS SSD"
   homepage "https://github.com/landxcape/mac-sym-offload"
-  version "0.4.0"
+  version "0.4.1"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/landxcape/mac-sym-offload/releases/download/v0.4.0/mso-macos-arm64.tar.gz"
-      sha256 "1c05d6d566ef16b0a8b6944454cfc016ed08ff233789a0da2de52de0d354d4a2"
+      url "https://github.com/landxcape/mac-sym-offload/releases/download/v0.4.1/mso-macos-arm64.tar.gz"
+      sha256 "aff089cb9dfc6a406ce537bd3dbcfce0940500e43d5632636f4c866fecb3e233"
     elsif Hardware::CPU.intel?
-      url "https://github.com/landxcape/mac-sym-offload/releases/download/v0.4.0/mso-macos-x86_64.tar.gz"
-      sha256 "60d8243e535a58518b9e7455206fdb8405ade9fb693e4f67b2a25b3421ad0a05"
+      url "https://github.com/landxcape/mac-sym-offload/releases/download/v0.4.1/mso-macos-x86_64.tar.gz"
+      sha256 "d1c721e6411c14ee2a034d1e07e646021f58cd95200817a88c18d6b52a238646"
     end
   end
 
@@ -19,6 +19,6 @@ class Mso < Formula
   end
 
   test do
-    assert_match "0.4.0", shell_output("#{bin}/mso --version")
+    assert_match "0.4.1", shell_output("#{bin}/mso --version")
   end
 end
